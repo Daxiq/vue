1d384818b205f779fdefbd0c29d19a4866240c23
Daxiq <37885077+Daxiq@users.noreply.github.com>
2019-08-15 17:34:07 -0600

    Changes in variables name

--- Vue/js/app.js
+++ Vue/js/app.js
@@ -2,45 +2,55 @@ const app = new Vue({
 	el: '#app',
 	data(){
 		return{
-			o_events: [],
+			o_events:[],
 			events:[],
 			event:[],
 			o_sessions:[],
-			sessions: [],
-			session: [],
-			o_registrations: [],
-			registrations: [],
+			sessions:[],
+			session:[],
+			o_registrations:[],
+			registrations:[],
 			type: '',
-			createForm: {
-				title : null,
-				description : null,
-				date : null,
-				time : null,
-				duration_days : null,
-				location : null,
-				standard_price : null,
-				capacity : null,
+			search: '',
+			createForm:{
+				// Events
+				title: null,
+				description: null,
+				date: null,
+				time: null,
+				duration_days: null,
+				location: null,
+				standard_price: null,
+				capacity: null,
+				// Sessions
+				event_id: null,
+				room: null,
+				speaker: null,
 			},
-			updateForm: {
+			updateForm:{
 				id: null,
-				title : null,
-				description : null,
-				date : null,
-				time : null,
-				duration_days : null,
-				location : null,
-				standard_price : null,
-				capacity : null,
-
-				session_title : null,
+				// Events
+				title: null,
+				description: null,
+				date: null,
+				time: null,
+				duration_days: null,
+				location: null,
+				standard_price: null,
+				capacity: null,
+				// Sessions
+				session_title: null,
+				session_event_id: null,
 				session_room: null,
 				session_speaker: null,
+				// Registrations
+				event_rating: null,
 			},
 			registerForm:{
 				event_id: null,
-				registration_type: null,
+				registartion_type: null,
 				type: null,
-				registration_date: null,
+				registation_date: null,
 				calculate_price: null,
 				event_rating: null,
 			},
@@ -74,16 +84,16 @@ const app = new Vue({
 			axios
 			.get('http://localhost/PHPandJS813/public/api/registrations/'+id)
 			.then((res)=>{
-					this.o_registrations = res.data;
-					this.registrations = res.data
+				this.o_registrations = res.data;
+				this.registrations = res.data;
 			})
 			.catch((err)=>{
 				console.log(err.response.data);
 			});
 		},
-		showEventDetail(id){
+		showEventModal(id){
 			axios
-			.get('http://localhost/PHPandJS813/public/api/event/'+ id)
+			.get('http://localhost/PHPandJS813/public/api/event/'+id)
 			.then((res)=>{
 				this.o_events = res.data;
 				this.event = res.data;
@@ -91,33 +101,46 @@ const app = new Vue({
 			.catch((err)=>{
 				console.log(err.response.data);
 			});
-
 			$('#eventDetail').modal();
 		},
-		showSessionsModal(id){
+		showSessionModal(id){
 			axios
-			.get('http://localhost/PHPandJS813/public/api/sessions/'+id)
+			.get('http://localhost/PHPandJS813/public/api/session/'+id)
 			.then((res)=>{
 				this.o_sessions = res.data;
-				this.sessions = res.data;
+				this.session = res.data;
 			})
 			.catch((err)=>{
 				console.log(err.response.data);
 			});
-			$('#sessionModal').modal();
+			$('#sessionDetail').modal();
 		},
-		showRegisterModal(event, id){
+		showCreateRegistration(event, id){
 			axios
-			.get('http://localhost/PHPandJS813/public/api/event/'+ id)
-			.then((res) => {
+			.get('http://localhost/PHPandJS813/public/api/event/'+id)
+			.then((res)=>{
 				this.o_events = res.data;
 				this.event = res.data;
 			})
-			.catch((err) =>{
+			.catch((err)=>{
 				console.log(err.response.data);
 			});
 
 			this.createForm.user_id = user_id_value.value;
+			this.createForm.event_id = id;
+			$('#registerEvent').modal();
+		},
+		showCreateSession(id){
+			axios
+			.get('http://localhost/PHPandJS813/public/api/session/'+id)
+			.then((res)=>{
+				this.o_sessions = res.data;
+				this.session = res.data;
+			})
+			.catch((err)=>{
+				console.log(err.response.data);
+			});
+
 			this.createForm.event_id = id;
 
 			$('#registerEvent').modal();
@@ -165,15 +188,15 @@ const app = new Vue({
 			});
 
 			this.updateForm.session_title = session.title;
-			this.updateForm.session_room = session.room;
-			this.updateForm.session_speaker = session.speaker;
+			this.updateForm.session_room = session.title;
+			this.updateForm.session_sepaker = session.title;
 
-			$('#updateSession').modal();
+			$('#updateEvent').modal();
 		},
 		createEvent(){
 			axios
 			.post('http://localhost/PHPandJS813/public/api/event', this.createForm)
-			.then((res)=>{
+			.then((ress)=>{
 				this.setEvents();
 				$('#createEvent').modal('hide');
 			})
@@ -181,10 +204,34 @@ const app = new Vue({
 				console.log(err.response.data);
 			});
 		},
+		createSession(){
+			axios
+			.post('http://localhost/PHPandJS813/public/api/session', this.createForm)
+			.then((ress)=>{
+				this.setSessions();
+				$('#createSession').modal('hide');
+			})
+			.catch((err)=>{
+				console.log(err.response.data);
+			});
+		},
+		createRegistration(){
+			axios
+			.post('http://localhost/PHPandJS813/public/api/registration', this.createForm)
+			.then((ress)=>{
+				this.setRegistrations();
+			})
+			.catch((err)=>{
+				console.log(err.response.data);
+			});
+
+			alert('Register');
+			$('#registerEvent').modal('hide');
+		},
 		updateEvent(event){
 			axios
-			.put('http://localhost/PHPandJS813/public/api/event/' + this.event.id, this.updateForm)
-			.then((res)=>{
+			.put('http://localhost/PHPandJS813/public/api/event' + this.event.id, this.updateForm)
+			.then((ress)=>{
 				this.setEvents();
 				$('#updateEvent').modal('hide');
 			})
@@ -192,23 +239,10 @@ const app = new Vue({
 				console.log(err.response.data);
 			});
 		},
-		registerEvent(){
-			axios
-			.post('http://localhost/PHPandJS813/public/api/registration', this.createForm)
-			.then((res)=>{
-				this.setRegistrations();
-			})
-			.catch((err)=>{
-				console.log(err.response.data);
-			});
-
-			alert('Registered successfully');
-			$('#registerEvent').modal('hide');
-		},
 		updateSession(session){
 			axios
-			.put('http://localhost/PHPandJS813/public/api/session/' + this.session.id, this.updateForm)
-			.then((res)=>{
+			.put('http://localhost/PHPandJS813/public/api/session' + this.session.id, this.updateForm)
+			.then((ress)=>{
 				this.setSessions();
 				$('#updateSession').modal('hide');
 			})
@@ -216,28 +250,36 @@ const app = new Vue({
 				console.log(err.response.data);
 			});
 		},
+
 	},
 	watch:{
-		type(val){
-			this.registration_type = this.type;
-			this.createForm.calculate_price = this.event.standard_price * this.type;
+		search(val){
+			this.events = this.o_events.filter((event)=>{
+				let reg = new RegExp(val, 'i');
 
-			this.createForm.registration_type = this.type;
-		}
+				return reg.test(event.date);
+			});
+		},
+		type(){
+			this.registartion_type = this.type;
+			this.createForm.calculate_price  = this.event.standard_price * this.type;
+			this.createForm.registartion_type = this.type;
+		},
+
 	},
 	created(){
-		console.log('created');
+		console.log('created')
 	},
 	mounted(){
 		this.setEvents();
 		this.setSessions();
 		this.setRegistrations();
-		console.log('mounted');
+		console.log('mounted')
 	},
 	updated(){
-		console.log('updated');
+		console.log('updated')
 	},
 	destroyed(){
-		console.log('destroyed');
+		console.log('destroyed')
 	},
 });
